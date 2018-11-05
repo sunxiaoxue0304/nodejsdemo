48c16564a67ea65bb34ae7ec8eef79aa73b34275
sunxiaoxue0304 <597232104@qq.com>
2018-11-05 15:38:20 +0800

    07-module

---
+++ 07-module/02-export-object.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const pi = Math.PI;
+
+module.exports = {
+  diameter: (radius) => 2 * radius,
+  circumference: (radius) => pi * 2 * radius,
+  area: (radius) => pi * radius * radius
+};
+
+//module.exports.diameter = (radius) => 2 * radius;
+//module.exports.circumference = (radius) => pi * 2 * radius;
+//module.exports.area = (radius) => pi * radius * radius;
+
+console.dir(module);
